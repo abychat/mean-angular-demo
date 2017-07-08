c7ba0d0a8cbe5548ed85ff94c4f670c588ca2254
Abhishek Chaturvedi (A.C.) <achaturvedi@salesforce.com>
2017-07-08 13:34:56 -0700

    Added crypto library

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var session = require('express-session');
+var crypto = require("crypto");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
